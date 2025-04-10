e71296ee0fd8d8b538f1ded382a5d72d6409d623
mothukuru saikiran <obscuresai.com>
2025-04-10 17:04:02 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-SOURCE_DIR="/tmp/saikiran-logs"
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ ! -d $SOURCE_DIR ]
-then 
-    echo -e "$R $SOURCE_DIR does not exist $N"
-fi
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-
-while IFS= read -r line
+for saikiran in {1...1000}
 do
-    echo "Deleting file:: $line"
-    rm -rf $line
-done <<< $FILES_TO_DELETE
+    echo "Number is:: $saikiran"
+done
