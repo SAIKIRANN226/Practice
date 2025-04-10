40bf0441b590d4e8ad4c215b2893b015fdee7961
mothukuru saikiran <obscuresai.com>
2025-04-10 17:18:12 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-for saikiran in {1..100000}
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date)
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$Y Script started executing at $DATE $N"
+fi 
+
+
+for package in $@
 do
-    echo "Number is:: $saikiran"
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y
+    else
+        echo -e "$package is already installed so $Y.....SKIPPING $N"
+    fi 
 done
