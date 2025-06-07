8a5f8914e90131ea4082ab7d696717e514e9ea2f
mothukuru saikiran <obscuresai.com>
2025-06-07 20:44:44 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -8,30 +8,34 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
-    exit 1
-else
-    echo -e "$Y Script started executing at $DATE $N"
-fi 
-
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-        echo -e "$2.....$R FAILED $N"
-        exit 1
-    else
+        echo -e "$2....$R FAILED $N"
+        exit 12
+    else 
         echo -e "$2....$G SUCCESS $N"
     fi 
 }
 
-yum install nginx -y &>> /tmp/nginx.output
 
-VALIDATE $? "Installing nginx"
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 232
+else
+    echo -e "$Y Script started executing at $DATE $N"
+fi 
 
-yum install mysql -y &>> /tmp/mysql.output
 
-VALIDATE $? "Installing mysql"
-
-
+for package in $@
+do 
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y &>> /tmp/saikiran.output
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already installed so $Y.....SKIPPING $N"
+    fi 
+done
