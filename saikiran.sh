4c878c716243bf4adfda58d49add335fcdc5da5c
mothukuru saikiran <obscuresai.com>
2025-06-07 15:37:44 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -8,12 +8,13 @@ result=$(ls -la)
 
 echo "Files including hidden $result" > /tmp/saikiran.output
 
+saikiran=$(ls -ltr)
+
+echo "files timing wise $saikiran" > /tmp/sai.output
 
 status=$?
 
 echo "the status of the above command is $status"
 
 
-saikiran=$(ls -ltr)
 
-echo "files timing wise $saikiran" > /tmp/sai.output
