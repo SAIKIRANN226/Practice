6ea5742605f265aa7be4961fbdc04f6cabe28627
mothukuru saikiran <obscuresai.com>
2025-06-08 17:15:48 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -29,7 +29,7 @@ fi
 
 for package in $@
 do 
-    yum list installed $package
+    yum list installed $package &>> $LOGFILE
     if [ $? -ne 0 ]
     then 
         yum install $package -y &>> $LOGFILE
