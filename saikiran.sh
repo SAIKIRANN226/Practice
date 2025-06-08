684ed6451553d21770028aee41f6bdd8c64eb471
mothukuru saikiran <obscuresai.com>
2025-06-08 17:11:51 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -6,7 +6,20 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGFILE="/tmp/saikiran.output"
+exec &>$LOGFILE
+
 DATE=$(date)
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi 
+}
 
 if [ $ID -ne 0 ]
 then 
@@ -14,4 +27,16 @@ then
     exit 1
 else
     echo -e "$Y Script started executing at $DATE $N"
-fi 
+fi
+
+for package in $@
+do 
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already installed $Y......SKIPPING $N"
+    fi
+done
