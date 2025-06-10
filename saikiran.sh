8be8a0847faf9a6909c8d6da8dac26d623fe008b
mothukuru saikiran <obscuresai.com>
2025-06-10 19:22:48 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -4,6 +4,6 @@ Y="\e[33m"
 
 LOG_DIR="/home/centos/log_folder/"
 
-find "$LOG_DIR" -type f -name "*.log" -mtime +14 -exec rm -f {} \;
+find "$LOG_DIR" -type f -name "sample" -mtime +14 -exec rm -f {} \;
 
 echo -e "$Y Old .log files older than 14 days have been deleted from $LOG_DIR. $N"
