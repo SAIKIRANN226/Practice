2570574bd54cdcdb5e14639fe46af4d06d9bcbca
mothukuru saikiran <obscuresai.com>
2025-06-11 18:05:10 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -19,5 +19,5 @@ fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*log" -exec rm -f {} \;)
 
-echo -e "$Y Successfully deleted old log files $N"
+echo -e "$G Successfully deleted old log files $N"
 
