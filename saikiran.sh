9b61b5ddb25970c536a61c1a8e1758f3eeed8b07
mothukuru saikiran <obscuresai.com>
2025-06-25 19:34:36 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-
-SOURCE_DIR="/tmp/my-logs"
-
-if [ ! -d $SOURCE_DIR ]
-then 
-    echo -e "$R $SOURCE_DIR does not exist $N"
-    exit 1
-fi 
-
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log" -delete)
+files=$(ls -la)
+echo "Printed all the files including hidden $files"
