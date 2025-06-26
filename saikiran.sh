fa461835f45c79c115f3b99608f21469fc4ba041
mothukuru saikiran <obscuresai.com>
2025-06-26 12:01:38 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -3,4 +3,7 @@
 person1=$1
 person2=$2
 
-echo "$person1:: hi how are you $person2 i really miss you from past 4years i want to meet you"
+echo "$person1:: hi how are you $person2"
+echo "Please enter your username"
+echo "Please enter your password"
+read -s PASSWORD
