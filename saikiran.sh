ecf7eb8b46224c89b40ada8e27bc60af44459f45
mothukuru saikiran <obscuresai.com>
2025-06-26 13:41:40 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,41 +1,42 @@
-#/bin/bash
+#!/bin/bash
 
-ID=$(date)
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-DATE=$(date)
-LOGFILE="/tmp/msk-logs"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"  
 
-VALIDATE() {
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$2.....$R FAILED $N"
-        exit 1
+    then
+        echo -e "$2 ... $R FAILED $N"
     else
-        echo -e "$2.....$G SUCCESS $N"
-    fi 
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
 }
 
-if [ $(id -u) -ne 0 ]
-then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 else
-    echo -e "$Y Script started executing at $DATE $N"
-fi 
+    echo "You are root user"
+fi
 
 
-for software in $@
-do 
-    yum list installed $software
-    if [ $? -ne 0 ]
-    then 
-        yum install $software -y &>> "$LOGFILE"
-        VALIDATE $? "Installing $software"
+for package in $@ 
+do
+    yum list installed $package &>> $LOGFILE # Check installed or not
+    if [ $? -ne 0 ] # If not installed
+    then
+        yum install $package -y &>> $LOGFILE # Install the package
+        VALIDATE $? "Installation of $package" # Validate
     else
-        echo -e "$Y $software is already installed so....SKIPPING $N"
+        echo -e "$package is already installed ... $Y SKIPPING $N"
     fi
 done
