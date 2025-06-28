59db4b6c4994ac00fecea7afe560d52998ec7400
mothukuru saikiran <obscuresai.com>
2025-06-28 16:15:57 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -8,3 +8,6 @@ read -s Samyuktha
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
+
+
+echo "Print $Samyuktha"
