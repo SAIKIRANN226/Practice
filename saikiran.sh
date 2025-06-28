eee526ac457cc917cd261d963e582c7cfff78c58
mothukuru saikiran <obscuresai.com>
2025-06-28 16:37:05 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -6,6 +6,16 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi
+}
+
 if [ $ID -ne 0 ]
 then 
     echo -e "$R ERROR:: Please run the script with root user $N"
@@ -17,11 +27,9 @@ fi
 
 yum install mysql -y > output.text
 
-if [ $? -ne 0 ]
-then 
-    echo -e "$R Installing mysql is failed $N"
-    exit 1
-else
-    echo -e "$G Installing mysql is success $N"
-fi
+VALIDATE $? "Installing mysql"
+
+yum install postfix -y > output.text
+
+VALIDATE $? "Installing postfix"
 
