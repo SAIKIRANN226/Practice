a419c78cf7b54cbebfd7aa012dd202a9799d59cb
mothukuru saikiran <obscuresai.com>
2025-06-28 16:18:48 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-PERSON1=Saikiran 
-PERSON2=Samyuktha
+FRUITS=("Apple" "Banana" "Mango")
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-read -s Samyuktha
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
+echo "First value: ${FRUITS[0]}"
 
+echo "Second value: ${FRUITS[1]}"
 
-echo "Print $Samyuktha"
+echo "Second value: ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
