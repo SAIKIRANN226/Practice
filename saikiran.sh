04084f0518790e27fc8ca6779bd7ea71eb6f6adc
mothukuru saikiran <obscuresai.com>
2025-06-29 18:18:53 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
 
-SOURCE_DIR="/tmp/MSK-Logs"
+while IFS= read line
+do 
+    usage=$(echo $line | awk '{print $6F}')
+    partition=$(echo $line | awk '${print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then 
+        message+="High disk usage on $partition = $usage <br>"
+    fi
+done <<< $DISK_USAGE
 
-if [ ! -d $SOURCE_DIR ]
-then 
-    echo -e "$R $SOURCE_DIR does not exits $N"
-    exit 1
-else
-    echo -e "$Y Check wether your $SOURCE_DIR exists or not $N"
-fi 
-
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log" -delete)
+echo -e "Message = $message"
