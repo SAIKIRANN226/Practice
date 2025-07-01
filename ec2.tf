9d9793ec6d3b4c57f4e043c08ac941b547597add
mothukuru saikiran <obscuresai.com>
2025-07-01 18:04:29 +0530

    testing

--- ec2.tf
+++ ec2.tf
@@ -1,31 +1,8 @@
 resource "aws_instance" "web" {
-    ami = "ami-0b4f379183e5706b9"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.saikiran.id]
+    count = length(var.instance_names)
+    ami = var.ami_id
+    instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
     tags = {
-        Name = "MSK_Instance"
-        Environemnt = "dev"
-        Terraform = "true"
-        Component = "web"
-    }
-}
-
-
-resource "aws_security_group" "saikiran" {
-    name = "allo-saikiran"
-    description = "allowing all as of now"
-
-    ingress {
-    description = "TLS from VPC"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    }
-
-    egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+        Name = var.instance_names[count.index]
     }
 }
