4a567c06d98eb3c8c52b05144e07b3c0ff3a166c
mothukuru saikiran <obscuresai.com>
2025-07-25 15:34:03 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -2,7 +2,8 @@
 
 fruits=("mango" "banana" "apple" "grapes" "dragon")
 
+echo "Script name is $0"
+
 echo "the value of fruits is ${fruits[0]}"
 echo "the value of fruits is ${fruits[1]}"
 echo "the value of fruits is ${fruits[@]}"
-echo "the value of fruits is ${fruits[#]}"
