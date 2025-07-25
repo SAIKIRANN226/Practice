c51945f68c358fb09260bbf934916134b487ecbe
mothukuru saikiran <obscuresai.com>
2025-07-25 15:39:19 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
 
 Number=$1
@@ -11,5 +10,5 @@ then
     echo -e "$Y Given number is greater than 100 $N"
     exit 1
 else
-    echo "$R Given number is lessthan 100 $N"
+    echo -e "$R Given number is lessthan 100 $N"
 fi 
