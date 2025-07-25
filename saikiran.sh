0f13f095887fe8a6fd9c3ddff8e003d75afc6c38
mothukuru saikiran <obscuresai.com>
2025-07-25 15:47:55 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -5,10 +5,17 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 DATE=$(date)
 
-echo "Script name is $0 and started at $DATE"
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi 
+}
 
 if [ $ID -ne 0 ]
 then 
@@ -21,10 +28,8 @@ fi
 
 yum install mysql -y 
 
-if [ $? -ne 0 ]
-then 
-    echo -e "$R Installing  mysql is failed $N"
-    exit 232
-else
-    echo -e "$G Installing mysql is success $N"
-fi 
+VALIDATE $? "Installing mysql"
+
+yum install git -y
+
+VALIDATE $? "Installing git"
