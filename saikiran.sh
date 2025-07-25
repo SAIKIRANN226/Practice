1be00c848477d40b02bb18cb754df0e4e9e1f8f8
mothukuru saikiran <obscuresai.com>
2025-07-25 16:52:26 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-DATE=$(date)
-LOGFILE="/tmp/$0.log"
-
-VALIDATE() {
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2.....$R FAILED $N"
-        exit 12
-    else
-        echo -e "$2.....$G SUCCESS $N"
-    fi 
-}
-
-if [ $ID -ne 0 ]
-then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
-    exit 1
-else
-    echo -e "$Y Script started executing at $DATE $N"
-fi 
-
-
-yum install git -y &>> $LOGFILE
-
-VALIDATE $? "Installing git"
-
-yum install mysql -y &>> $LOGFILE
-
-VALIDATE $? "Installing mysql"
-
-yum install postfix -y &>> $LOGFILE
-
-VALIDATE $? "Installing postfix"
+for saikiran in {1..1000}
+do 
+    echo $saikiran
+done
