ac095d2d560fb917ee853a96fb92d232b4db8d2b
mothukuru saikiran <obscuresai.com>
2025-07-26 21:48:39 +0530

    testing

--- redis.sh
+++ redis.sh
@@ -27,7 +27,7 @@ else
     echo -e "$Y Script started executing at $DATE $N"
 fi 
 
-yum sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-8.5-2.el8.remi.noarch.rpm -y &>> $LOGFILE
+/usr/bin/dnf sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-8.5-2.el8.remi.noarch.rpm -y &>> $LOGFILE
 
 VALIDATE $? "Installing repo file"
 
