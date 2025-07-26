0a8da03d2ad6dba29587f3a6df2535ed8ba9c2c1
mothukuru saikiran <obscuresai.com>
2025-07-26 20:06:49 +0530

    testing

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date)
+LOGFILE="/tmp/$0.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$Y Script started executing at $DATE $N"
+fi 
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Giving remote access"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting mongodb"
