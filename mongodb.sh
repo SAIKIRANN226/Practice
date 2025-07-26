d691adac6fd151f6323b29cddae05a527248331c
mothukuru saikiran <obscuresai.com>
2025-07-26 20:10:34 +0530

    testing

--- mongodb.sh
+++ mongodb.sh
@@ -27,7 +27,7 @@ else
     echo -e "$Y Script started executing at $DATE $N"
 fi 
 
-cp /home/centos/mongodb.repo /etc/yum.repos.d/mongodb.repo &>> $LOGFILE
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>> $LOGFILE
 
 VALIDATE $? "Copied mongodb file"
 
