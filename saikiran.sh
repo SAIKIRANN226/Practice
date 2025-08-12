4023fd1abac416fc8ce5429f093e6562f28c6c94
mothukuru saikiran <obscuresai.com>
2025-08-12 13:59:01 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-for i in {1..10000}
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DATE=$(date)
+LOGFILE="/tmp/msk.log"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.....$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi 
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: Please run the script with root user $N"
+    exit 1
+else
+    echo -e "$Y Script started executing at $DATE $N"
+fi 
+
+for package in $@ 
 do 
-    echo "Number $i"
+    yum list $package -y &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then 
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? "Installing $package"
+    else 
+        echo -e "$package is already installed......$Y SO SKIPPING $N"
+    fi
 done 
