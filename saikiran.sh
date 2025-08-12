4802455a5880af93984f51f0312706f11dadc797
mothukuru saikiran <obscuresai.com>
2025-08-12 14:29:55 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -6,35 +6,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-DATE=$(date)
-LOGFILE="/tmp/msk.log"
+echo "Fetching the list of s3 buckets..."
 
-VALIDATE() {
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2.....$R FAILED $N"
-        exit 1
-    else
-        echo -e "$2.....$G SUCCESS $N"
-    fi 
-}
-
-if [ $ID -ne 0 ]
+aws sts get-caller-identity >/dev/null 2>&1
+if [ $? -ne 0 ]
 then 
-    echo -e "$R ERROR:: Please run the script with root user $N"
+    echo -e "$R AWS_CLI is not configured properly. Run aws configure first $N"
     exit 1
-else
-    echo -e "$Y Script started executing at $DATE $N"
 fi 
 
-for package in $@ 
-do 
-    yum list installed $package -y &>> $LOGFILE
-    if [ $? -ne 0 ]
-    then 
-        yum install $package -y &>> $LOGFILE
-        VALIDATE $? "Installing $package"
-    else 
-        echo -e "$package is already installed......$Y SO SKIPPING $N"
-    fi
-done 
+aws s3 ls 
+
+echo "Done"
