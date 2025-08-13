1ff209a7f78d8dbd287965fbcd52f40aad747196
mothukuru saikiran <obscuresai.com>
2025-08-13 14:05:30 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+AMI="ami-0b4f379183e5706b9"
+SG="sg-062184d660bab16ba"
+INSTANCES=("mongodb", "shipping", "web", "payment")
+ZONE_ID="Z03123921NOU39UU26DXY"
 
-SOURCE_DIR="/tmp/saikiran-logs"
-
-if [ ! -d $SOURCE_DIR ]
-then 
-    echo -e "$R Source directory $SOURCE_DIR does not exits $N"
-    exit 1
-fi 
-
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -type d -mtime +7 -name "preethi")
-
-rmdir -p
+for i in "${INSTANCES[@]}"
+do 
+    if [ $i == "mongodb"] || [ $i == "shipping"]
+    then 
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+    echo "PublicIP of the $i: $IP_ADDRESS"
+done 
