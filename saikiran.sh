12b118de2e08aa07dea8978c432ff791c8d4ce48
mothukuru saikiran <obscuresai.com>
2025-08-13 12:20:25 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -18,6 +18,6 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line 
 do 
-    echo "Deleting line: $line"
+    echo "Deleting file: $line"
     rm -rf $line 
 done <<< $FILES_TO_DELETE
