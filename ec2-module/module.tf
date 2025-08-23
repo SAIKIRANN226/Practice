b08211d865dbef66e629c81f1e08b949da543ab7
mothukuru saikiran <obscuresai.com>
2025-08-23 19:15:12 +0530

    testing

--- ec2-module/module.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "saikiran" {
-  ami = var.ami_id
-  instance_type = var.instance_type
-  tags = var.tags
-}
--- roboshop/roboshop.tf
+++
@@ -1,5 +0,0 @@
-module "saikiran" {
-    source = "../ec2-module"
-    instance_type = "t3.small"
-    tags = var.tags 
-}
