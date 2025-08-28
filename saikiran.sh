04c926b95520b8f7b51b7a525109bebe3b2b22aa
mothukuru saikiran <obscuresai.com>
2025-08-28 14:05:43 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -6,6 +6,16 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+DATE=$(date)
+
+if [ $ID -ne 0 ]
+then 
+  echo -e "$R ERROR:: Please run the script with root user $N"
+  exit 1
+else
+  echo -e "$Y Script started executing at $DATE $N"
+fi 
+
 yum install nginx -y 
 if [ $? -ne 0 ]
 then 
