8e90e5b34a810c550606d6e2b3924117418385cc
mothukuru saikiran <obscuresai.com>
2025-08-28 14:26:05 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -7,8 +7,26 @@ Y="\e[33m"
 N="\e[0m"
 
 DATE=$(date)
+LOGFILE="/tmp/$0.log"
 
-for i in {1..1000}
+VALIDATE() {
+  if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 ... $R FAILED $N"
+    exit 1
+  else 
+    echo -e "$2 ... $G SUCCESS $N"
+  fi 
+}
+
+for package in $@ 
 do 
-  echo "Number $i"
-done 
+  yum list installed $package -y &>> $LOGFILE
+  if [ $? -ne 0 ]
+  then 
+    yum install $package -y &>> $LOGFILE
+    VALIDATE $? "Installing $package"
+  else
+    echo -e "$Y $package is already installed so skipping $N"
+  fi 
+done
