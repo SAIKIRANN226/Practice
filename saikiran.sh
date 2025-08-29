d8cb8f4e397bbb5ab771b6bc60b10461b35d6576
mothukuru saikiran <obscuresai.com>
2025-08-29 13:08:43 +0530

    testing

--- saikiran.sh
+++ saikiran.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 
-ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-DATE=$(date)
-LOGFILE="/tmp/$0.log"
+SOURCE_DIR="/tmp/sai.log"
 
-VALIDATE() {
-  if [ $1 -ne 0 ]
-  then 
-    echo -e "$2 ... $R FAILED $N"
-    exit 1
-  else 
-    echo -e "$2 ... $G SUCCESS $N"
-  fi 
-}
-
-if [ $ID -ne 0 ]
+if [ ! -d $SOURCE_DIR ]
 then 
-  echo -e "$R ERROR:: Please run the script with root user $N"
+  echo -e "$R $SOURCE_DIR does not exist $N"
   exit 1
-else
-  echo -e "$Y Script started executing at $DATE $N"
 fi 
 
-for package in $@ 
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line 
 do 
-  yum list installed $package -y &>> $LOGFILE
-  if [ $? -ne 0 ]
-  then 
-    yum install $package -y &>> $LOGFILE
-    VALIDATE $? "Installing $package"
-  else
-    echo -e "$Y $package is already installed so skipping $N"
-  fi 
-done
+  echo "Deleting line $line"
+  rm -rm $line
+done <<< $FILES_TO_DELETE
