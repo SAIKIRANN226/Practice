5c1f1aeb18c275852035d34be1a14237c92b9e50
mothukuru saikiran <obscuresai.com>
2025-09-27 15:41:58 +0530

    practice

--- saikiran.sh
+++ saikiran.sh
@@ -5,16 +5,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "$Y Fetching the list of s3 buckets...$N"
+File="/etc/passwd"
 
-aws sts get-caller-identity 
-
-if [ $? -ne 0 ]
-then 
-    echo -e "$R AWS_CLI is not configured properly. Configure aws first $N"
-    exit 1
-fi 
-
-aws s3 ls 
-
-echo -e "$G Done $N"
+while IFS= read -r line 
+do 
+    echo -e "$Y $line $N"
+done <<< $File
